2f35dae283ee033e4443b5436be6d4359aea1707
leamaysara <lea.schmidt6@outlook.fr>
2023-10-20 11:45:50 +0200

    last changes

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const slides = [ //Définit une variable slides qui est un tableau d'objets. Chaque objet représente une diapositive du carrousel et contient une image et une ligne de texte associée.//
+const slides = [
 
 	{
 		"image":"slide1.jpg",
@@ -18,48 +18,53 @@ const slides = [ //Définit une variable slides qui est un tableau d'objets. Cha
 	}
 ]
 
-let bannerImg = document.querySelector(".banner-img")
-let bannerTxt = document.querySelector("#banner p")
-let dots = document.querySelector(".dots")
-let slideNbr = 0 //initialise une variable slideNbr à 0, qui représentera l'index de la diapositive actuellement affichée.//
+let bannerImg = document.querySelector(".banner-img");
+let bannerTxt = document.querySelector("#banner p");
+let dots = document.querySelector(".dots");
+let slideNbr = 0; // Initializes a variable slideNbr to 0, which represents the index of the currently displayed slide.//
 
-//Bullet points pour chaque diapo: boucle 'for' suivante ajoute un élément '<div>' avec la classe "dot" à l'intérieur de l'élément avec la classe "dots" pour chaque élément dans le tableau slides.//
+function updateBannerContent() {
+    bannerImg.src = `./assets/images/slideshow/${slides[slideNbr].image}`; // Updates source. //
+    bannerTxt.innerHTML = `${slides[slideNbr].tagLine}`; // Updates the HTML content with the text line.//
+}
+
+// Bullet points for each slide: The following 'for' loop adds a '<div>' element with the class "dot" inside the element with the class "dots" for each element in the slides array.//
 for (let i=0; i<slides.length; i++) {
 	let dot = '<div class="dot"></div>'
 	dots.innerHTML += dot
 }
 let dot = document.querySelectorAll(".dot")
-console.log(dot) //Affiche les bullet points dans la console du navigateur.//
-dot[0].classList.add("dot_selected") //Ajoute la classe "dot_selected" au premier bullet point pour le marquer comme actif.//
-
+console.log(dot)
+dot[0].classList.add("dot_selected") // Adds the "dot_selected" class to the first bullet point to mark it as active.//
 const arrowLeft = document.querySelector(".arrow_left")
 
-//Ajoute un écouteur d'événements qui réagit lorsque l'utilisateur clique sur la flèche gauche. Le code à l'intérieur de la fonction gère le changement de diapositive lorsque la flèche gauche est cliquée.//
 arrowLeft.addEventListener("click", () => {
 	dot[slideNbr].classList.remove("dot_selected")
+	
 	if (slideNbr===0){
 		slideNbr = slides.length - 1
 	} else {
 		slideNbr--
 	}
-	bannerImg.src= `./assets/images/slideshow/${slides[slideNbr].image}`
-	bannerTxt.innerHTML=`${slides[slideNbr].tagLine}`
+
+	updateBannerContent();
 	
-	dot[slideNbr].classList.add("dot_selected")  //Ajoute la classe "dot_selected" au bullet point correspondant à la diapositive actuellement sélectionnée, ce qui le met en surbrillance visuellement.//
+	dot[slideNbr].classList.add("dot_selected")  // Adds the "dot_selected" class to the bullet point corresponding to the currently selected slide, highlighting it.//
 })
 
 const arrowRight = document.querySelector(".arrow_right")
 
-//Idem event listener arrow right//
 arrowRight.addEventListener("click", () => {
-	dot[slideNbr].classList.remove("dot_selected") //retire la classe "dot_selected" du bullet point sélectionné: le point ne sera plus en surbrillance.//
-	if (slideNbr===slides.length - 1){ //Condition 'if' qui vérifie si nous sommes à la dernière diapositive du carrousel. slides.length - 1 renvoie l'index de la dernière diapositive, car les indices commencent à partir de 0.//
-		slideNbr = 0 //Si dernière diapo, cette ligne réinitialise.//
-	} else {
-		slideNbr++ //Si la condition dans le 'if' n'est pas remplie (càd si nous ne sommes pas à la dernière diapositive), alors slideNbr + 1, ce qui nous amène à la diapositive suivante.//
-	}	
-	bannerImg.src= `./assets/images/slideshow/${slides[slideNbr].image}` //Met à jour la source de l'image de la bannière (bannerImg) avec le chemin de l'image de la diapositive actuellement sélectionnée: change l'image affichée.//
-	bannerTxt.innerHTML=`${slides[slideNbr].tagLine}` //Met à jour le contenu HTML à l'intérieur de l'élément avec l'ID "banner" (bannerTxt) avec la ligne de texte associée à la diapositive actuellement sélectionnée.//
+	dot[slideNbr].classList.remove("dot_selected") // Removes the "dot_selected" class from the selected bullet point: the point will no longer be highlighted.//
 	
+	if (slideNbr===slides.length - 1){ // 'if' condition checks if we are on the last slide of the carousel. slides.length - 1 returns the index of the last slide, as indices start from 0.//
+		slideNbr = 0 // If it's the last slide, this line resets it.//
+	} else {
+		slideNbr++ // If the condition in the 'if' statement is not met (i.e., if we are not on the last slide), then slideNbr + 1, which takes us to the next slide.//
+	}	
+
+	updateBannerContent(); 
+
 	dot[slideNbr].classList.add("dot_selected")
 })
+
